0f967189a2640e05bb931958d22ee100265d1cff
ddb4 <ddb4@tpu.ru>
2023-11-02 12:01:10 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import {Suspense} from "react";
+import {Link, Route, Routes} from 'react-router-dom'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {UseTheme} from "./theme/useTheme";
+import './styles/index.scss'
+import {classNames} from "./helpers/classNames/classNames";
+
+const App = () => {
+    const { theme, toggleTheme } = UseTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Change theme</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                        <Route path={'/'} element={<MainPageAsync />} />
+                        <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(arg: number): string {
+    console.log('RANDOM FUNCTION')
+    console.log(arg)
+    return 'Test1'
+}
